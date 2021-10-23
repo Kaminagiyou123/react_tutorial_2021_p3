c8426ea7c5b263f57cb868d07ccda1692c4dc612
Ran You <ran.you8311@gmail.com>
2021-10-23 10:55:22 -0700

    Project 8 complete

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(number);
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (amount > data.length) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <>
+      <section className='section-center'>
+        <h3>tired of borning lorem ipsum?</h3>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='amoun'>paragraphs:</label>
+          <input
+            type='number'
+            name='amount'
+            id='amount'
+            value={number}
+            onChange={(e) => {
+              setNumber(e.target.value);
+            }}
+          />
+          <button type='submit' className='btn'>
+            generate
+          </button>
+        </form>
+        <article className='lorem-text'>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default App;
