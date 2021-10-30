11073a3612cae7d7dbfd4d0dffad4c671f083d3d
Ran You <ran.you8311@gmail.com>
2021-10-30 11:54:43 -0700

    cockail project complete

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      about us
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit
+        vel debitis consequuntur voluptate, sit modi nesciunt odit molestiae
+        soluta qui necessitatibus, facere officia earum deleniti. Enim tenetur
+        libero illum repellendus?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
-}
+    <section className='error-page section'>
+      <h1>opps! it is a dead end</h1>
+      <Link to='/' className='btn btn-primary'>
+        back home
+      </Link>
+    </section>
+  );
+};
 
-export default Error
+export default Error;
