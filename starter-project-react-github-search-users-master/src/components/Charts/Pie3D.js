892deb6b53517f6b602ca672dc1ea8b313f8ad8b
Ran You <ran.you8311@gmail.com>
2021-11-10 20:05:19 -0800

    part 4 complete

--- starter-project-react-github-search-users-master/src/components/Charts/Pie3D.js
+++ starter-project-react-github-search-users-master/src/components/Charts/Pie3D.js
@@ -24,16 +24,16 @@ ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 const ChartComponent = ({ data }) => {
   const chartConfigs = {
     type: "pie3d", // The chart type
-    width: "400", // Width of the chart
+    width: "100%", // Width of the chart
     height: "400", // Height of the chart
     dataFormat: "json", // Data type
     dataSource: {
       // Chart Configuration
       chart: {
-        caption: "Languages",
+        //Set the chart caption
+        caption: "Lanuages",
+
         theme: "fusion",
-        decimals: 0,
-        pieRadius: "45%",
       },
       // Chart Data
       data,
--- starter-project-react-github-search-users-master/src/components/Repos.js
+++ starter-project-react-github-search-users-master/src/components/Repos.js
@@ -5,24 +5,49 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   let languages = repos.reduce((total, item) => {
-    const { language } = item;
+    const { language, stargazers_count } = item;
     if (!language) return total;
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
 
     return total;
   }, {});
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages).sort((a, b) => {
+    return b.value - a.value;
+  });
+  //most stars per language
+
+  const mostPopular = Object.values(languages)
     .sort((a, b) => {
-      return b.value - a.value;
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
     })
     .slice(0, 5);
+
+  //star,forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
   const chartData = [
     {
       label: "HTML",
@@ -41,7 +66,11 @@ const Repos = () => {
   return (
     <section className='section'>
       <Wrapper className='section-center'>
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+        <div></div>
       </Wrapper>
     </section>
   );
