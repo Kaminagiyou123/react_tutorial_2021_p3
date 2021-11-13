c916f07e619db7908a1fc2055f7abe6d28a6e366
Ran You <ran.you8311@gmail.com>
2021-11-13 09:48:05 -0800

    Whole Project complete!

--- starter-project-react-github-search-users-master/src/index.js
+++ starter-project-react-github-search-users-master/src/index.js
@@ -5,12 +5,20 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+//dev-lu9u0upp.us.auth0.com
+//7tel90H64IcoRTHPk97qxIvBMfr00Siw
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-lu9u0upp.us.auth0.com'
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
